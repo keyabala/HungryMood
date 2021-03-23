79fb35c676045d55edc1b927ffebae9577ee77ef
kayabala9@gmail.com <Meiskk@123>
2021-03-23 18:33:01 +0600

    Commit

---
+++ app/src/main/java/com/example/hungrymood/SplashScreen.java
@@ -0,0 +1,23 @@
+package com.example.hungrymood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    ImageView splashImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        splashImage = (ImageView) findViewById(R.id.iv_top);
+
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.animation);
+    }
+}
