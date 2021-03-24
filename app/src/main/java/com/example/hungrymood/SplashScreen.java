57a419aeeb41457d6236cf981a98da0923c6b702
kayabala9@gmail.com <Meiskk@123>
2021-03-24 13:12:42 +0600

    works on splash-screen

--- app/src/main/java/com/example/hungrymood/SplashScreen.java
+++ app/src/main/java/com/example/hungrymood/SplashScreen.java
@@ -2,10 +2,15 @@ package com.example.hungrymood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+
+import java.time.Instant;
+
+import static android.os.SystemClock.sleep;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -19,5 +24,26 @@ public class SplashScreen extends AppCompatActivity {
         splashImage = (ImageView) findViewById(R.id.iv_top);
 
         Animation animation = AnimationUtils.loadAnimation(this,R.anim.animation);
+        splashImage.startAnimation(animation);
+
+        Thread myThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+
+                    Thread.sleep(10000);
+                    Intent i = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(i);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+//                sleep(5000);
+
+            }
+        });
+
+        myThread.start();
     }
 }
