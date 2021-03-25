a1b328e366917a7b7e334f1872e2f01ec9127d89
kayabala9@gmail.com <Meiskk@123>
2021-03-25 18:13:44 +0600

    works on hungryMood

--- app/src/main/java/com/example/hungrymood/MainActivity.java
+++ app/src/main/java/com/example/hungrymood/MainActivity.java
@@ -3,9 +3,13 @@ package com.example.hungrymood;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +21,14 @@ public class MainActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
         );
         getSupportActionBar().hide();
+
+        login = findViewById(R.id.btnLogIn);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "", Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
