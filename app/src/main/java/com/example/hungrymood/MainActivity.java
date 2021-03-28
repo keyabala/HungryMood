277bf9df6d67d8b534437937832fb618c1e5a99b
kayabala9@gmail.com <Meiskk@123>
2021-03-28 15:50:09 +0600

    works on Green-Cafe

--- app/src/main/java/com/example/hungrymood/MainActivity.java
+++ app/src/main/java/com/example/hungrymood/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.hungrymood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
@@ -9,7 +10,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Button login;
+    Button login , signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +24,24 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 
         login = findViewById(R.id.btnLogIn);
+        signup = findViewById(R.id.btnRegister);
+
+
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Login has been clicked!!!", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+                startActivity(intent);
+
+            }
+        });
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
 
             }
         });
